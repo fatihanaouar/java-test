8c7f4300f7818374f44e7bfce136957f86542057
root <root@localhost>
2026-04-16 23:55:05 +0200

    Ajouter la multiplication dans App.java

--- App.java
+++ App.java
@@ -1,7 +1,9 @@
 public class App {
     public static void main(String[] args) {
-        int a = Integer.parseInt(args[0].trim()); // trim() : supprimer lesespaces invisibles 
+        int a = Integer.parseInt(args[0].trim());
         int b = Integer.parseInt(args[1].trim());
+
         System.out.println("Somme = " + (a + b));
+        System.out.println("Produit = " + (a * b));
     }
 }
